6ec26c51211fa433ebf8fb00372e6abbef9327b0
Harish SC <harishsc@Harishs-MacBook-Pro.local>
2022-08-08 23:00:21 +0530

    More junits added

--- src/test/java/com/harish/library/controller/test/BookSearchControllerTest.java
+++ src/test/java/com/harish/library/controller/test/BookSearchControllerTest.java
@@ -1,5 +1,7 @@
 package com.harish.library.controller.test;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import java.util.HashSet;
@@ -11,6 +13,7 @@ import org.junit.Test;
 import org.junit.rules.ExpectedException;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -78,7 +81,7 @@ public class BookSearchControllerTest {
 		// Act and Assert
 		mockMvc.perform(MockMvcRequestBuilders.get(url)).andExpect(status().isOk());
 	}
-	
+
 	@Test
 	public void testSearchBooksByAttribute_SearchWithTitleAndTag() throws Exception {
 		// Given
@@ -94,9 +97,55 @@ public class BookSearchControllerTest {
 		mockMvc.perform(MockMvcRequestBuilders.get(url)).andExpect(status().isOk());
 	}
 
-//	@Test
-//	public void testSearchBooksByAuthorId_NoBooksFound_ThrowsNoResultsFoundException() throws Exception {
-//		String url = "/api/v1/author/" + 1 + "/books";
-//		mockMvc.perform(MockMvcRequestBuilders.get(url)).andExpect(status().isOk());
-//	}
+	@Test
+	public void testSearchBooksByAttribute_SearchWithTag() throws Exception {
+		// Given
+		String url = "/api/v1/search/books?tag=fiction";
+		Set<Book> books = new HashSet<>();
+		books.add(new Book(isbn, "Harry Potter"));
+		when(bookSearchService.searchBooksByTag("fiction")).thenReturn(books);
+
+		// Act and Assert
+		mockMvc.perform(MockMvcRequestBuilders.get(url)).andExpect(status().isOk());
+		verify(bookSearchService, times(1)).searchBooksByTag("fiction");
+		verify(bookSearchService, times(0)).searchBooksByTitle(Mockito.anyString());
+		verify(bookSearchService, times(0)).searchBookByISBN(Mockito.anyString());
+		verify(bookSearchService, times(0)).searchBooksByAuthorName(Mockito.anyString());
+		verify(bookSearchService, times(0)).searchBooksByAuthorId(Mockito.any());
+	}
+
+	@Test
+	public void testSearchBooksByAttribute_SearchWithTitleAndAuthorName() throws Exception {
+		// Given
+		String url = "/api/v1/search/books?title=Believer&tag=Life";
+		Set<Book> books = new HashSet<>();
+		books.add(new Book(isbn, "Harry Potter"));
+		when(bookSearchService.searchBooksByTitle("Believer")).thenReturn(books);
+		when(bookSearchService.searchBooksByTag("Life")).thenReturn(books);
+
+		// Act and Assert
+		mockMvc.perform(MockMvcRequestBuilders.get(url)).andExpect(status().isOk());
+		verify(bookSearchService, times(1)).searchBooksByTitle("Believer");
+		verify(bookSearchService, times(0)).searchBookByISBN(Mockito.anyString());
+		verify(bookSearchService, times(1)).searchBooksByTag("Life");
+		verify(bookSearchService, times(0)).searchBooksByAuthorId(Mockito.any());
+	}
+
+	@Test
+	public void testSearchBooksByAttribute_SearchWithISBNAndAuthorId() throws Exception {
+		// Given
+		String url = "/api/v1/search/books?author_id=123&isbn=777-3-55-248511-2";
+		Set<Book> books = new HashSet<>();
+		books.add(new Book(isbn, "Harry Potter"));
+		when(bookSearchService.searchBooksByAuthorId(123L)).thenReturn(books);
+		when(bookSearchService.searchBookByISBN("777-3-55-248511-2"))
+				.thenReturn(new Book("777-3-55-248511-2", "Harish's Biography"));
+
+		// Act and Assert
+		mockMvc.perform(MockMvcRequestBuilders.get(url)).andExpect(status().isOk());
+		verify(bookSearchService, times(0)).searchBooksByTitle(Mockito.anyString());
+		verify(bookSearchService, times(1)).searchBookByISBN("777-3-55-248511-2");
+		verify(bookSearchService, times(0)).searchBooksByTag(Mockito.anyString());
+		verify(bookSearchService, times(1)).searchBooksByAuthorId(123L);
+	}
 }
